d435346c53841da7380252ad37f30e74e87d9793
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2026-03-25 22:28:12 -0300

    numeros

---
+++ teo-me-why/2-tipos-basicos/numeros/numeros.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("1 + 1 =", 1+1)
+	fmt.Println("1 + 2.5 =", 1+2.5)
+	fmt.Println("10 / 3 =", 10/3)
+	fmt.Println("10 / 3.0 =", 10/3.0)
+	fmt.Println("100 * 7.5 =", 100/7.5)
+	fmt.Println("5 % 3=", 5%3)
+}
