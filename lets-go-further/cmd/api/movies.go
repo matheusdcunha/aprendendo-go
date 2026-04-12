fc365f8b93ae7d5219786b8b2c9223b1ce2782a0
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2026-04-12 13:17:59 -0300

    cria validator para movies

--- lets-go-further/cmd/api/movies.go
+++ lets-go-further/cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"greenlight.matheusdcunha.cloud/internal/data"
+	"greenlight.matheusdcunha.cloud/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +21,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
