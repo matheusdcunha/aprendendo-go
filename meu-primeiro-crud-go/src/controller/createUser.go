f5a8a9dc4bbc65c3c40f84de7ee68b447822239b
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2026-04-19 12:47:46 -0300

    feat: adiciona os models para request e response de user.

--- meu-primeiro-crud-go/src/controller/createUser.go
+++ meu-primeiro-crud-go/src/controller/createUser.go
@@ -1,13 +1,32 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/matheusdcunha/aprendendo-go/meu-primeiro-crud-go/src/config/resterr"
+	"github.com/matheusdcunha/aprendendo-go/meu-primeiro-crud-go/src/controller/model/request"
+	"github.com/matheusdcunha/aprendendo-go/meu-primeiro-crud-go/src/controller/model/response"
 )
 
 func CreateUser(c *gin.Context) {
 
-	err := resterr.NewBadRequestError("implementation not finished")
+	var userRequest request.UserRequest
 
-	c.JSON(err.Code, err)
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		resterr := resterr.NewBadRequestError(
+			fmt.Sprintf("there are some incorrect fields, error=%s", err.Error()))
+
+		c.JSON(resterr.Code, resterr)
+		return
+	}
+
+	userResponse := response.UserResponse{
+		Id:    "1",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	c.JSON(201, userResponse)
 }
