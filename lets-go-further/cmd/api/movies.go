230cb87e0fd5eda8938b2e1d6aab587c4e68c16c
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2026-04-22 20:35:16 -0300

    feat: adiciona novo routing e helper para ler o ID dos parametros

---
+++ lets-go-further/cmd/api/movies.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
+}
--- lets-go-further/go.mod
+++ lets-go-further/go.mod
@@ -1,3 +1,5 @@
 module github.com/matheusdcunha/aprendendo-go/lets-go-further
 
 go 1.26.1
+
+require github.com/julienschmidt/httprouter v1.3.0
