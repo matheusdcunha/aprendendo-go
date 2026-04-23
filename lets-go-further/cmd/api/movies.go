3a8fc5a84c025d5b2c13397f9cd3321865d144df
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2026-04-23 18:58:55 -0300

    feat: adiciona helper de padronizacao de error

--- lets-go-further/cmd/api/movies.go
+++ lets-go-further/cmd/api/movies.go
@@ -17,8 +17,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
-		return
+		app.notFoundResponse(w, r)
 	}
 
 	movie := data.Movie{
@@ -32,7 +31,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
