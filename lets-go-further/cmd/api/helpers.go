2afa1f2b079b412fa5229c7bea6cc33f5fd9ecfe
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2026-04-23 18:13:31 -0300

    feat: adiciona encapsulamento nas respostas

--- lets-go-further/cmd/api/helpers.go
+++ lets-go-further/cmd/api/helpers.go
@@ -9,6 +9,8 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type envelope map[string]any
+
 func (app *application) readIDParam(r *http.Request) (int, error) {
 	parms := httprouter.ParamsFromContext(r.Context())
 
@@ -20,7 +22,7 @@ func (app *application) readIDParam(r *http.Request) (int, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 
 	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
